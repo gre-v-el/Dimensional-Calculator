56906be712cbee7110756c8d4ca21931a3d63753
gre-v-el <myszkier.gabriel@gmail.com>
2024-03-01 15:00:09 +0100

    Set up typescript.

---
+++ docs/main.js
@@ -0,0 +1,6 @@
+"use strict";
+function calculate() {
+    var input = document.getElementById("input").value;
+    var output = document.getElementById("output");
+    output.innerHTML = "Hi, " + input + "!";
+}
